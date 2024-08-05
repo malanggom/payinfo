d3f603202f82ea9df9d42cc342a7db3d42666cde
손승연 <kds.ssy98@gmail.com>
2024-08-05 20:20:52 +0900

    "모달 추가"

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,6 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
     path: '/devinq',
     name: 'devinq',
     component: () => import('../views/DevMng/DevInqView.vue')
