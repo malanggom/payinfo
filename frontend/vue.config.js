496020c0af4af0cd237f2a1840461a0b634000be
손승연 <kds.ssy98@gmail.com>
2025-12-29 20:15:54 +0900

    1. vue.config.js sourcemap 을 꺼야함.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,12 +4,13 @@ const webpack = require('webpack');
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  productionSourceMap: false,   // ⭐ 핵심
+
   configureWebpack: {
-    devtool: 'source-map',
     plugins: [
       new webpack.DefinePlugin({
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
       }),
     ],
-  }
+  },
 });
